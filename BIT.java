072da5d984eb2478abc59d84c2d377855ee175c4
Ayush_sccg <ayush1240945@gmail.com>
2023-02-23 04:24:41 +0545

    a

---
+++ BIT.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BIT {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int[] arr = new int[10000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    }
+}
